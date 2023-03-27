a63c1779a009d8c68cbd10bb7c2b25fc3eba4e61
Ceren Susuz <ceren199704@hotmail.com>
2023-03-27 18:05:19 +0300

    task10-done ok

---
+++ Shop Inventory/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace shopinventory
+{
+    class MainClass
+    {
+        static Shop shop = new();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Add your inventory items:");
+
+            for (int index = 0; index < 3; index++)
+            {
+                var item = Console.ReadLine();
+                AddItem(index, item);
+            }
+
+            Console.WriteLine("Retrieve all stored items:");
+            GetAllItems();
+        }
+
+        public static void AddItem(int index, string name)
+        {
+            try
+            {
+                shop[index] = name;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void GetAllItems()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    Console.WriteLine(shop[i]);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
